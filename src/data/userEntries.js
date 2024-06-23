d2fe0d1412631b0a64c8120f9051833aa8117065
ngeTaco <thealdo.531@gmail.com>
2024-06-23 12:23:58 -0600

    Boiler Plate Static Site

---
+++ src/data/userEntries.js
@@ -0,0 +1,37 @@
+const entryData = {
+    animeData: [
+        {
+            id: 0,
+            title_eng: 'ONE PIECE',
+            title_jpn: 'ONE PIECE',
+            season: 'Fall',
+            seasonYear: 1999,
+            episodes: 1110,
+            coverImage: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx21-YCDoj1EkAxFn.jpg',
+            selected_episode: 37,
+            blog_text: 'this is such a great episode'
+        },
+        {
+            id: 0,
+            title_eng: 'Neon Genesis Evangelion',
+            title_jpn: 'Shin Seiki Evangelion',
+            season: 'Fall',
+            seasonYear: 1995,
+            episodes: 26,
+            coverImage: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx30-wmNoX3m2qTzz.jpg',
+            selected_episode: 24,
+            blog_text: 'Where did the animation go?'
+        },
+        {
+            id: 0,
+            title_eng: 'Fullmetal Alchemist: Brotherhood',
+            title_jpn: 'Hagane no Renkinjutsushi: FULLMETAL ALCHEMIST',
+            season: 'Spring',
+            seasonYear: 2009,
+            episodes: 64,
+            coverImage: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx5114-Dilr312jctdJ.jpg',
+            selected_episode: 4,
+            blog_text: 'A real 2 for 1 special. I really like the part where the guy got punched with a metal fist.'
+        },
+    ],
+};
